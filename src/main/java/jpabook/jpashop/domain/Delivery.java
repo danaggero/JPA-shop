74a48ffc7175fcfb66bedea2832a1b1f08a8c4b0
이지원 <daggero@naver.com>
2025-10-06 23:03:27 +0900

    feat: Entity 클래스 개발1

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,4 +1,21 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.*;
+
 public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long deliveryId;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // READY, COMP
+
+
 }
